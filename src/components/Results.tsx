885f2d329caee0ec5dfcc7dc2d962b8384eee2ca
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-05 13:55:07 +0300

    Implemented -- incase there's no age value

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -7,13 +7,13 @@ const Results = () => {
   return (
     <div className="flex flex-col gap-x-[10px] px-[50px] text-[60px] font-extrabold pb-[50px]">
       <p className="">
-        <span className="text-Purple">{age.years}</span> years
+        <span className="text-Purple">{age.years || "--"}</span> years
       </p>
       <p className="">
-        <span className="text-Purple">{age.months}</span> months
+        <span className="text-Purple">{age.months || "--"}</span> months
       </p>
       <p className="">
-        <span className="text-Purple">{age.days}</span> days
+        <span className="text-Purple">{age.days || "--"}</span> days
       </p>
     </div>
   );
