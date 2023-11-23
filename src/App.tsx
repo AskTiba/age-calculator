cdd9450c8c4ad00e396c3054387898b49216dbfc
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-23 16:34:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -32,10 +32,10 @@ function App() {
     <>
       <div
         className="bg-Light-grey flex justify-center 
-      items-center min-h-screen min-w-full h-[600px]"
+      items-center h-screen min-w-full"
       >
         <div
-          className="bg-White flex flex-col  
+          className="bg-White flex flex-col sm:block sm:relative
         rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] rounded-br-[25%] "
         >
           <DateContext.Provider value={{ date, setDate }}>
