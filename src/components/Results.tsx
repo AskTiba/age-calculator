899bc8bac8aa349e1a684a842cdfb86d26c85d2d
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-23 20:17:19 +0300

    Update Results.tsx

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -5,15 +5,15 @@ const Results = () => {
   const { age } = useContext(AgeContext); // Replace AgeContext with the actual context you're using
   console.log(age.year);
   return (
-    <div className="flex flex-col gap-x-[10px] px-[50px] text-[80px]  pb-[50px]">
+    <div className="flex flex-col gap-x-[10px] px-[50px] text-[80px] font-extrabold pb-[50px]">
       <p className="">
-        <span className="text-Purple ">{age.years || "--"}</span> years
+        <span className="text-Purple ">{age?.years || "--"}</span> years
       </p>
       <p className="">
-        <span className="text-Purple">{age.months || "--"}</span> months
+        <span className="text-Purple">{age?.months || "--"}</span> months
       </p>
       <p className="">
-        <span className="text-Purple">{age.days || "--"}</span> days
+        <span className="text-Purple">{age?.days || "--"}</span> days
       </p>
     </div>
   );
