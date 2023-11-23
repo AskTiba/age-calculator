85e114f6244d9375e2b97bb43989dd707157162b
AskTiba <132336450+AskTiba@users.noreply.github.com>
2023-11-23 11:07:01 +0300

    Update Button.tsx
    Added the hover functionality

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -29,7 +29,11 @@ const Button = () => {
     <div className="flex items-center px-[50px]">
       <hr className="border border-Light-grey w-[570px]" />
       <button className="" onClick={handleCalculate}>
-        <img className="bg-Purple rounded-full p-[12px]" src={icon} alt="" />
+        <img
+          className="bg-Purple rounded-full p-[12px] hover:bg-[--Off-black]"
+          src={icon}
+          alt=""
+        />
       </button>
     </div>
   );
